4e0400faaf27c9f1f14c14a89c6a87a32deb50fc
unknown <SkoneMR@LNGSEAL-7000201.legal.regn.net>
2013-01-11 11:35:23 -0800

    Added environment configuration sniffing

--- index.js
+++ index.js
@@ -3,10 +3,15 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Welcome to BSA Tree Recycle!');
 });
 
-var port = 9999; //process.env.PORT || 5000;
+if(app.settings.env == 'production') {
+  var port = process.env.PORT || 5000;  
+} else {
+  var port = 9999;
+}
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
