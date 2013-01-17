56a65aad2feceab7e2257949f5600dab58b6684c
unknown <SkoneMR@LNGSEAL-7000201.legal.regn.net>
2013-01-16 16:40:28 -0800

    Implemented server events and jquery listener

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ var app = express();
 app.configure(function() {
   app.set('view engine', 'jade');
   app.set('views', __dirname + '/views');
-  app.use(express.static(__dirname + '/static'));
+  app.use(express.bodyParser());
+  app.use(express.logger('tiny'));
+  app.use(express.static('images'));
+  app.use(express.static('scripts'));
 });
 
 if(app.settings.env == 'production') {
@@ -15,13 +18,82 @@ if(app.settings.env == 'production') {
 }
 
 app.get('/', function(request, response) {
-  //response.send('Welcome to BSA Tree Recycle!');
   response.render('index');
+});
+
+var connections = [];
+
+app.get('/events', function(req, res) {
+  //connections.push(res);
+  //console.log('Connections: ' + connections.length);
+  
+  //res.writeHead(200, {
+  //  'Content-Type': 'text/event-stream',
+  //  'Cache-Control': 'no-cache',
+  //  'Connection': 'keep-alive'
+  //});
+  
+  //sendEvent('First call');
+});
+
+app.get('/createmarker', function(req, res) {
+  connections.push(res);
+  console.log('Connections: ' + connections.length);
+  
+  res.writeHead(200, {
+    'Content-Type': 'text/event-stream',
+    'Cache-Control': 'no-cache',
+    'Connection': 'keep-alive',
+    'Spam': 'eggs'
+  });
+});
+
+app.get('/sendevent', function(req, res) {
+  //sendEvent('Click!');
+  res.send(200);
 });
 
 app.get('/map', function(req, res) {
   res.render('map');
 });
+
+app.get('/jquerytest', function(req, res) {
+  res.send(req.url);
+});
+
+var times = new Array();
+
+app.post('/jquerytest', function(req, res) {
+  times.push(req.body.time);
+  res.send(times);
+});
+
+app.post('/createmarker', function(req, res) {
+  var latlng = 'data: ' + JSON.stringify({"lat":req.body.lat,"lng":req.body.lng}) + '\n\n';
+  console.log(latlng);
+  sendMarker(latlng);
+  res.redirect('..');
+});
+
+function sendMarker(data) {
+  if(data != null) {
+    connections.forEach(function(element, index, array) {
+      element.write(data);
+    });
+  };
+};
+
+function sendEvent(data) {
+  if(data != null) {
+    var event = 'data: ' + data + '\n\n';
+    console.log(event);
+    connections.forEach(function(element, index, array) {
+      element.write(event);
+    });
+  } else {
+    return null;
+  };
+};
 
 app.listen(port, function() {
   console.log("Listening on " + port);
