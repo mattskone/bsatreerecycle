609cd37a9f50042426e761f358f524d30c6da9dd
? <matt.skone@gmail.com>
2013-02-26 09:57:06 -0800

    Added shim for Array.forEach() support in IE.  Fixes #11.

--- scripts/map.js
+++ scripts/map.js
@@ -7,6 +7,14 @@ var map,
     selectedDate,
     today = new Date();
 
+if ( !Array.prototype.forEach ) {
+  Array.prototype.forEach = function(fn, scope) {
+    for(var i = 0, len = this.length; i < len; ++i) {
+      fn.call(scope, this[i], i, this);
+    }
+  }
+}
+
 $(document).ready(function() {
     cookieZoom = /zoom=\d+/.exec(document.cookie);
     initialZoom = cookieZoom ? Number(cookieZoom[0].slice(5)) : 4;
